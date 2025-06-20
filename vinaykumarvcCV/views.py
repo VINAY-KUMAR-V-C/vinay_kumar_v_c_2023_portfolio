cf05a94b00395227edfad568e94731e176ab9019
Vinay Kumar V C <84114504+VINAY-KUMAR-V-C@users.noreply.github.com>
2025-06-20 12:52:37 +0530

    Update views.py

--- vinaykumarvcCV/views.py
+++ vinaykumarvcCV/views.py
@@ -18,7 +18,7 @@ class CompleteInfo():
                 'link': 'https://www.google.com/maps/place/Bangalore%2C%20Karnataka',
             },
             'description': 'Software engineer ,@ ZOHO CRM',
-            'CVLink':'static/image/VinayKumarVC2025.pdf',
+            'CVLink':'static/image/VinayKumarVC.pdf',
             'profilePhoto':{
                 'fromStatic' : 'static/image/IMG_2907.jpg',
                 'fromRepo' : 'https://avatars.githubusercontent.com/u/84114504?v=4'
